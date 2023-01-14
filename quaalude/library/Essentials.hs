a51fb3b2976d8c5f007bb09946f3ce0feaee0cb5
Chris Martin <ch.martin@gmail.com>
2023-01-14 12:58:24 -0700

    line up exports, add arrow ops

--- quaalude/library/Essentials.hs
+++ quaalude/library/Essentials.hs
@@ -1,25 +1,26 @@
 module Essentials
   (
-    {- * Functions -} ($), (&), (.),
-    {- * Functor -} fmap, (<$>), (<&>), (<$), ($>), void,
-    {- * Applicative -} pure, (<*>), (<*), (*>),
-    {- * Monad -} (>>=), (=<<), (>=>), (<=<),
-    {- * Bool -} Bool (False, True), otherwise,
-    {- * Comparison -} (==), (/=), (<), (>), (<=), (>=),
-    {- * Monoid -} (<>), mempty,
-    {- * Numbers -} Natural, Integer, Int, (+), (-),
-    {- * Traversal -} traverse, traverse_,
-    {- * Maybe -} Maybe (Nothing, Just), maybe,
-    {- * Void -} Void, absurd,
-    {- * Identity -} Identity (Identity, runIdentity),
-    {- * Const -} Const (Const, getConst),
-    {- * Type classes -} Semigroup, Monoid, Eq, Ord, Enum, Bounded, Show,
+    {- * Functions           -} ($), (&), (.), (>>>), (<<<),
+    {- * Functor             -} fmap, (<$>), (<&>), (<$), ($>), void,
+    {- * Applicative         -} pure, (<*>), (<*), (*>),
+    {- * Monad               -} (>>=), (=<<), (>=>), (<=<),
+    {- * Bool                -} Bool (False, True), otherwise,
+    {- * Comparison          -} (==), (/=), (<), (>), (<=), (>=),
+    {- * Monoid              -} (<>), mempty,
+    {- * Numbers             -} Natural, Integer, Int, (+), (-),
+    {- * Traversal           -} traverse, traverse_,
+    {- * Maybe               -} Maybe (Nothing, Just), maybe,
+    {- * Void                -} Void, absurd,
+    {- * Identity            -} Identity (Identity, runIdentity),
+    {- * Const               -} Const (Const, getConst),
+    {- * Type classes        -} Semigroup, Monoid, Eq, Ord, Enum, Bounded, Show,
     {- * Constructor classes -} Functor, Applicative, Monad, Foldable, Traversable,
-    {- * Undefined -} undefined,
+    {- * Undefined           -} undefined,
   )
   where
 
 import Control.Applicative   (Applicative, pure, (*>), (<*), (<*>))
+import Control.Arrow         ((>>>), (<<<))
 import Control.Monad         (Monad, (<=<), (=<<), (>=>), (>>=))
 import Data.Bool             (Bool (False, True), otherwise)
 import Data.Eq               (Eq, (/=), (==))
