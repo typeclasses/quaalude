5c9e7938cc9a2197318785e67ce99aac7fbf1ccf
Chris Martin <ch.martin@gmail.com>
2023-01-15 01:03:26 -0700

    nix infra

---
+++ default.nix
@@ -0,0 +1,35 @@
+let
+
+sources = import ./nix/sources.nix;
+nixos-22-11 = import sources."nixos-22.11" {};
+inherit (nixos-22-11) haskell lib symlinkJoin;
+inherit (lib) fold composeExtensions concatMap attrValues;
+
+combineOverrides = old:
+    fold composeExtensions (old.overrides or (_: _: { }));
+
+sourceOverrides = haskell.lib.packageSourceOverrides {
+    quaalude = ./quaalude;
+};
+
+depOverrides = new: old: {
+    # package-name = new.callPackage ./nix/package-name-0.0.0.0.nix {};
+};
+
+ghc."9.2" = nixos-22-11.haskell.packages.ghc92.override (old: {
+    overrides = combineOverrides old [ sourceOverrides depOverrides ];
+});
+
+ghc."9.4" = nixos-22-11.haskell.packages.ghc94.override (old: {
+    overrides = combineOverrides old [ sourceOverrides depOverrides ];
+});
+
+in
+
+symlinkJoin {
+    name = "quaalude";
+    paths = concatMap (x: [x.quaalude]) (attrValues ghc);
+} // {
+    inherit ghc;
+    pkgs = nixos-22-11;
+}
