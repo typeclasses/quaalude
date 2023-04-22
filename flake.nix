5ebe496a29d7bd274aec89dfd1ecc9d11056dcf9
Chris Martin <ch.martin@gmail.com>
2023-04-22 01:00:13 -0600

    flake and ghc 9.6

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "quaalude";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils }:
+    let packageName = "quaalude";
+    in flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pkgsUnstable = import nixpkgs-unstable { inherit system; };
+
+        project = pkgs.haskellPackages.developPackage {
+          root = ./quaalude;
+          name = packageName;
+        };
+
+        inherit (pkgs.lib) fold composeExtensions concatMap attrValues;
+
+        combineOverrides = old:
+          fold composeExtensions (old.overrides or (_: _: { }));
+
+      in {
+        defaultPackage = self.packages.${system}.${packageName};
+
+        packages = {
+          "${packageName}" = project;
+
+          testConfigurations = let
+
+            inherit (pkgs.haskell.lib) dontCheck;
+            makeTestConfiguration = let defaultPkgs = pkgs;
+            in { pkgs ? defaultPkgs, ghcVersion, overrides ? new: old: { } }:
+            let inherit (pkgs.haskell.lib) dontCheck packageSourceOverrides;
+            in (pkgs.haskell.packages.${ghcVersion}.override (old: {
+              overrides = combineOverrides old [
+                (packageSourceOverrides { quaalude = ./quaalude; })
+                (new: old: { })
+                overrides
+              ];
+            })).quaalude;
+          in rec {
+            ghc-9-2 = makeTestConfiguration { ghcVersion = "ghc92"; };
+            ghc-9-4 = makeTestConfiguration { ghcVersion = "ghc94"; };
+            ghc-9-6 = makeTestConfiguration {
+              ghcVersion = "ghc96";
+              pkgs = pkgsUnstable;
+            };
+            all = pkgs.symlinkJoin {
+              name = "quaalude";
+              paths = [ ghc-9-2 ghc-9-4 ghc-9-6 ];
+            };
+          };
+        };
+      });
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
-To test, run `nix-build`. This will build and test the package with all
-supported GHC and dependency versions.
+To build and test:
+
+    nix build .#testConfigurations.all --no-link
